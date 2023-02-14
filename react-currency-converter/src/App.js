035d14a928afa29e9c136aebe87e4efc7ac447da
akrhode <a.rhode@web.de>
2023-02-15 00:58:19 +0100

    Style converter, add image

--- react-currency-converter/src/App.js
+++ react-currency-converter/src/App.js
@@ -1,7 +1,16 @@
+import React from "react";
 import "./App.css";
+import CurrencyRow from "./CurrencyRow.js";
 
 function App() {
-  return <h1>Currency Converter</h1>;
+  return (
+    <>
+      <h1>Currency Converter</h1>
+      <CurrencyRow />
+      <div className="equals">=</div>
+      <CurrencyRow />
+    </>
+  );
 }
 
 export default App;
--- react-currency-converter/src/index.js
+++ react-currency-converter/src/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import currencies from "./img/currencies.jpg";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <div
+      className="image"
+      style={{
+        backgroundImage: `url(${currencies})`,
+        backgroundRepeat: "no-epeat",
+        backgroundSize: "contain",
+        backgroundImage: `linear-gradient( rgba(8, 8, 37, 0.5), rgba(0, 15, 37, 0.7)), url("${currencies}")`,
+      }}
+    >
+      <App />
+    </div>
   </React.StrictMode>
 );
